03cb3e450d118199207b39dbf67ef3ebc611cec5
fimreal <6913651@qq.com>
2022-08-17 10:07:03 +0800

    add httpdo

--- http/http.go
+++ http/http.go
@@ -40,7 +40,9 @@ func HttpBufGet(url string) (result string, err error) {
 	return
 }
 
-/* SimplePost 简单 post 调用，内部做了 header 处理和返回错误判断
+/*
+	SimplePost 简单 post 调用，内部做了 header 处理和返回错误判断
+
 用法举例：
 url := "http://example.com"
 headers := make(map[string]string)
@@ -100,4 +102,22 @@ func Httpc(url string, method string, data []byte, headers map[string]string) ([
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	return body, err
+}
+
+/*
+defer resp.Body.Close()
+
+body, err := ioutil.ReadAll(resp.Body)
+*/
+func HttpDo(url string, method string, data []byte, headers map[string]string) (*http.Response, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, strings.NewReader(string(data)))
+	if err != nil {
+		return nil, err
+	}
+	for key, header := range headers {
+		req.Header.Set(key, header)
+	}
+	return client.Do(req)
 }
