cbd2eb1abc62b79f18942094a46ec23a41eb591b
fimreal <6913651@qq.com>
2022-11-08 17:27:45 +0800

    add check lanip

--- net/ip.go
+++ net/ip.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"net"
+	"strings"
 
 	"github.com/fimreal/goutils/ezap"
 )
@@ -38,4 +39,19 @@ func IsIPv6(ip string) bool {
 	ezap.Info(v6)
 
 	return v6 != nil
+}
+
+func IsLanIPv4(ip string) bool {
+	if !IsIPv4(ip) {
+		return false
+	}
+
+	lanex := []string{"10.", "192.168.", "172.16", "172.17", "172.18", "172.19", "172.20", "172.21", "172.22", "172.23", "172.24", "172.25", "172.26", "172.27", "172.28", "172.29", "172.30", "172.31"}
+
+	for _, lan := range lanex {
+		if strings.HasPrefix(ip, lan) {
+			return true
+		}
+	}
+	return false
 }
--- net/ip_test.go
+++ net/ip_test.go
@@ -33,3 +33,13 @@ func TestIsIPv6(t *testing.T) {
 		t.Error("err")
 	}
 }
+
+func TestIsLanIPv4(t *testing.T) {
+	ips := []string{"10.10.0.1", "192.168.0.1", "172.16.1.1", "172.31.7.7"}
+
+	for _, ip := range ips {
+		if !IsLanIPv4(ip) {
+			t.Error("err")
+		}
+	}
+}
